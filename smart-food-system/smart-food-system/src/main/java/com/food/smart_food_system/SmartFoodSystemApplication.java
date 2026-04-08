2ab21b8cac7d00a6a643f054c85d6630179f5857
letienbao69 <tienbao37fc@gmail.com>
2026-04-08 21:04:08 +0700

    init

---
+++ smart-food-system/smart-food-system/src/main/java/com/food/smart_food_system/SmartFoodSystemApplication.java
@@ -0,0 +1,13 @@
+package com.food.smart_food_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFoodSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFoodSystemApplication.class, args);
+	}
+
+}
---
+++ smart-food-system/smart-food-system/src/test/java/com/food/smart_food_system/SmartFoodSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.smart_food_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFoodSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
