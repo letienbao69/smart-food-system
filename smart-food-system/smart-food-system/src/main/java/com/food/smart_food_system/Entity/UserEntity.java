9a72583676edfcf076ac08dd06ea21994c975bfd
hoanghieu24 <162785962+hoanghieu24@users.noreply.github.com>
2026-04-09 10:04:26 +0700

    Update pom

--- smart-food-system/smart-food-system/src/main/java/com/food/smart_food_system/Entity/UserEntity.java
+++ smart-food-system/smart-food-system/src/main/java/com/food/smart_food_system/Entity/UserEntity.java
@@ -1,13 +1,14 @@
 package com.food.smart_food_system.Entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
-public class UserEntity  extends BaseEntity {
-
+public class UserEntity extends BaseEntity {
 
     @Column(name = "full_name", nullable = false)
     private String fullName;
@@ -20,47 +21,7 @@ public class UserEntity  extends BaseEntity {
 
     @Column(name = "password", nullable = false)
     private String password;
-    private String avatarUrl;
-
-    public String getAvatarUrl() {
-        return avatarUrl;
-    }
-
 
     @Column(name = "avatar_url")
-    public void setAvatarUrl(String avatarUrl) {
-        this.avatarUrl = avatarUrl;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+    private String avatarUrl;
 }
